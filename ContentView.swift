2a383fdd826b6b4d1c87beb962d4560769f1a9fb
jayzou3773 <133576930+jayzou3773@users.noreply.github.com>
2023-09-11 12:16:23 -0700

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  QingFood
+//
+//  Created by 吴卓航 on 2023/6/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            OnBoarding()
+            //ImagePickView()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QingFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  QingFoodApp.swift
+//  QingFood
+//
+//  Created by 吴卓航 on 2023/6/3.
+//
+
+import SwiftUI
+
+@main
+struct QingFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
